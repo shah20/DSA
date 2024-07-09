e22ded5d0d62e0f2eaaf50e518129063926d3c5e
Rushabh Shah <rushabh.shah@powerschool.com>
2024-07-09 22:09:51 +0530

    BST: added Breath First Search

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -110,6 +110,23 @@ class BST {
     deleteNode(value) {
         this.root = this.#deleteNode(value, this.root);
     }
+
+    BFS() {
+        let currentNode = this.root;
+        let queue = [];
+        let result = [];
+
+        queue.push(currentNode);
+
+        while(queue.length) {
+            currentNode = queue.shift();
+            result.push(currentNode.value);
+            if(currentNode.left) queue.push(currentNode.left);
+            if(currentNode.right) queue.push(currentNode.right);
+        }
+
+        return result;
+    }
 }
 
 let myTree = new BST();
