91f7b99e89df5b3f88cfb35f7d36ae08bc6da3ad
Rushabh Shah <rushabh.shah@powerschool.com>
2024-07-09 21:55:36 +0530

    BST: added recursive implementation of delete

--- BinarySearchTree.js
+++ BinarySearchTree.js
@@ -75,6 +75,41 @@ class BST {
         if(this.root === null) this.root = new Node(value);
         this.#rInsert(value);
     }
+
+    minValue(currentNode) {
+        while(currentNode.left !== null) {
+            currentNode = currentNode.left;
+        }
+        return currentNode.value;
+    }
+
+    #deleteNode(value, currentNode) {
+        if(currentNode === null) return null;
+        
+        if(value < currentNode.value) {
+            currentNode.left = this.#deleteNode(value, currentNode.left);
+        } else if(value > currentNode.value) {
+            currentNode.right = this.#deleteNode(value, currentNode.right);
+        } else {
+            if(currentNode.left === null && currentNode.right === null) {
+                return null;
+            } else if(currentNode.left === null) {
+                currentNode = currentNode.right;
+            } else if(currentNode.right === null) {
+                currentNode = currentNode.left;
+            } else {
+                let subTreeMin = this.minValue(currentNode.right);
+                currentNode.value = subTreeMin;
+                currentNode.right = this.#deleteNode(subTreeMin, currentNode.right);
+            }
+        }
+
+        return currentNode;
+    }
+
+    deleteNode(value) {
+        this.root = this.#deleteNode(value, this.root);
+    }
 }
 
 let myTree = new BST();
